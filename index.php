43d93bbfd62ce193a00bae648d153f3599ba55d0
Pau111111 <paugarciapellicer96@gmail.com>
2022-08-29 17:41:08 +0200

    Solved syntax error

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once "config/constants.php";
 include_once "config/db.php";
 
 if (isset($_GET['controller'])) {
-    $controller = CONTROLLERS . $_GET['controller' . "Controller.php";
+    $controller = CONTROLLERS . $_GET['controller'] . "Controller.php";
     $fileExists = file_exists($controller);
     if ($fileExists) {
         require_once $controller;
